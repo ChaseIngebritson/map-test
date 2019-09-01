6ef11b6eca1c70a81da945b795aed53d1e125ff7
Chase Ingebritson <chase.ingebritson2014@gmail.com>
2019-08-31 23:19:34 -0500

    Got a single tile working

--- src/app/viewer/viewer.page.ts
+++ src/app/viewer/viewer.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import mapboxgl from 'mapbox-gl';
-import { Scene, PerspectiveCamera, PlaneGeometry, WebGLRenderer } from 'three';
+import { Scene, PerspectiveCamera, PlaneGeometry, WebGLRenderer, DoubleSide, MeshBasicMaterial, Mesh, Vector3, TextureLoader } from 'three';
+import OrbitControls from 'three-orbitcontrols';
+import tilebelt from '@mapbox/tilebelt';
 
 @Component({
   selector: 'app-viewer',
@@ -12,31 +13,19 @@ import { Scene, PerspectiveCamera, PlaneGeometry, WebGLRenderer } from 'three';
 export class ViewerPage implements OnInit {
 
   token = 'pk.eyJ1IjoiZ29tb25rZXlhbWFuZ28iLCJhIjoiY2pneHgwOTNtMXA1eTMzcGd1eHVtMXYxeiJ9.ERQfx8U8kHKB8adxfGkMGA';
-  map: any;
   scene: Scene;
   renderer: WebGLRenderer;
   camera: PerspectiveCamera;
+  controls: OrbitControls;
   @ViewChild('rendererContainer', { static: true }) rendererContainer: ElementRef;
   currentLocation: { latitude: number, longitude: number };
 
-  constructor(private http: HttpClient, private geolocation: Geolocation) {}
+  constructor(private geolocation: Geolocation) {}
 
   ngOnInit() {
     mapboxgl.accessToken = this.token;
-    this.map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v9'
-    });
-
-    this.map.on('load', () => {
-      this.map.addSource('dem', {
-        type: 'raster-dem',
-        url: 'mapbox://mapbox.terrain-rgb'
-      });
-    });
 
     // Initialize Three constants
-    /*
     const width = window.innerWidth;
     const height = window.innerHeight;
     const viewAngle = 45;
@@ -50,9 +39,10 @@ export class ViewerPage implements OnInit {
     this.renderer.setSize(width, height);
     this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
 
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
     // Begin animation
     this.animate();
-    */
 
     // Test with the users current location
     this.testTerrainRgb();
@@ -62,16 +52,71 @@ export class ViewerPage implements OnInit {
     return -10000 + ((r * 256 * 256 + g * 256 + b) * 0.1);
   }
 
-  async getTerrainRgb(latitude: number, longitude: number, zoom: number) {
-    const url = `https://api.mapbox.com/v4/mapbox.terrain-rgb/${zoom}/${latitude}/${longitude}.pngraw?access_token=${this.token}`;
-    return this.http.get(url).toPromise();
+  getTileUrl(latitude: number, longitude: number, zoom: number) {
+    let [x, y, z] = tilebelt.pointToTile(longitude, latitude, zoom)
+    return `https://api.mapbox.com/v4/mapbox.terrain-rgb/${z}/${x}/${y}.pngraw?access_token=${this.token}`;
+  }
+
+  getPixels(url: string): Promise<Uint8ClampedArray> {
+    return new Promise ((resolve, reject) => {
+      const canvas = document.createElement('canvas');
+      const context = canvas.getContext('2d');
+  
+      const img = new Image();
+      img.crossOrigin = 'Anonymous';
+      img.src =  url;
+  
+      img.onload = () => {
+        canvas.width = img.width;
+        canvas.height = img.width;
+    
+        context.drawImage(img, 0, 0, img.width, img.width);
+    
+        const imgData = context.getImageData(0, 0, img.width, img.height);
+
+        resolve(imgData.data)
+      }
+    });
   }
 
   async testTerrainRgb() {
     await this.getUserLocation();
 
-    const rgb = await this.getTerrainRgb(this.currentLocation.latitude, this.currentLocation.longitude, 15);
-    console.log(rgb);
+    const tileUrl = await this.getTileUrl(this.currentLocation.latitude, this.currentLocation.longitude, 15);
+    const pixels = await this.getPixels(tileUrl);
+
+    const planeSize = Math.sqrt(pixels.length / 4);
+
+    const geometry = new PlaneGeometry(planeSize, planeSize, planeSize - 1, planeSize - 1);
+
+    for (let i = 0; i < pixels.length; i += 4) {
+      let r = pixels[i + 0];
+      let g = pixels[i + 1];
+      let b = pixels[i + 2];
+
+      const height = this.rgbToHeight(r, g, b);
+
+      if (!geometry.vertices[i/4]) {
+        console.error(`No vertices at index ${i/4} found.`);
+        break;
+      }
+      geometry.vertices[i/4].z = height;
+    }
+    
+    geometry.verticesNeedUpdate = true;
+    
+    const texture = new TextureLoader().load(tileUrl);
+    const material = new MeshBasicMaterial({ map: texture, side: DoubleSide, wireframe: true });
+    const mesh = new Mesh(geometry, material);
+
+    this.scene.add(mesh);
+
+    this.scene.updateMatrixWorld(true);
+    const position = new Vector3();
+    position.setFromMatrixPosition(mesh.matrixWorld);
+
+    this.camera.position.set(position.x, position.y, 500)
+    this.controls.update();
   }
 
   animate() {
